8cfe3d8c0b6961bd874ef998fe9b8429ef495214
bre-nda <brendaandeso4@gmail>
2023-08-24 03:33:18 -0700

    update the gallery page

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-// Smooth scroll function
-function smoothScroll(target) {
-  const targetSection = document.querySelector(target);
-  if (targetSection) {
-    window.scrollTo({
-      top: targetSection.offsetTop,
-      behavior: 'smooth'
-    });
-  }
+const carousel = document.querySelector(".carousel");
+firstImg = document.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
+
+
+
+
+const showHideIcons = () => {
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-// Event listeners for scrolling to sections
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('section-1').addEventListener('click', () => smoothScroll('#section-1'));
-  document.getElementById('section2').addEventListener('click', () => smoothScroll('#section3'));
-  // Add more event listeners for additional sections as needed
+arrowIcons.forEach( icon => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14;
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60);
+  });
 });
+
+const autoSlide = () => {
+  console.log(positionDiff);
+
+}
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+}
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+  autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
